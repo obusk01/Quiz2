daaa69be1498d65492707634150fe749819c9127
Olivia Busk <123026517+obusk01@users.noreply.github.com>
2023-02-03 11:44:27 -0500

    Add files via upload

---
+++ Quiz2.py
@@ -0,0 +1,15 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# Olivia Busk
+# Quiz 2
+
+# problem 5
+str(42)
+'42'
+
+#problem 6
+int('Sunshine')
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    int('Sunshine')
+ValueError: invalid literal for int() with base 10: 'Sunshine'
